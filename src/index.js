fbdeb820b3c8188ac672e48ab5d8737b3f5945b1
Amitesh-exp <amitesh2878@gmail.com>
2026-05-13 09:41:41 +0530

    Added airplanes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index')
 const db = require('./models/index')
+const {Airplane} = require('./models/index')
 
 const  setupAndStartServer = async () => {
 
@@ -13,7 +14,7 @@ const  setupAndStartServer = async () => {
     app.use(bodyParser.urlencoded({extended: true})); 
 
     app.use('/api', ApiRoutes);
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server Started at ${PORT}`); 
         if (process.env.SYNC_DB) db.sequelize.sync({alter: true})
     });
